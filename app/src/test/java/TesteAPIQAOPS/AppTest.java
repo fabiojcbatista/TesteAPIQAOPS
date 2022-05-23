9fbe90d6b50a2e83d8d1f9831a60757ca3e7d867
fabiojcbatista <fabiojcbweb@gmail.com>
2022-05-23 17:07:24 -0300

    Inserindo dependência rest assured e função listar usuários

--- app/src/test/java/TesteAPIQAOPS/AppTest.java
+++ app/src/test/java/TesteAPIQAOPS/AppTest.java
@@ -1,10 +1,25 @@
 package TesteAPIQAOPS;
 
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 
 public class AppTest {
     @Test
-    public void appHasAGreeting() {
+    public void testListaMetadadosDoUsuario() {
 
+        when()
+                .get("https://reqres.in/api/users?page=2")
+               .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("data", is(notNullValue()))
+                // .body("page", is(2))
+                // .body("per_page", is(6))
+                // .body("data[0].email", is("michael.lawson@reqres.in"))
+                // .body("data[0].first_name", is("Michael"))
+                .statusCode(200);
     }
 }
