6b20c040e9e5cf7e140748d4dfbd37d62b2fcfd7
fabiojcbatista <fabiojcbweb@gmail.com>
2022-05-23 17:24:59 -0300

    Insere usuário com sucesso

--- app/src/test/java/TesteAPIQAOPS/AppTest.java
+++ app/src/test/java/TesteAPIQAOPS/AppTest.java
@@ -1,11 +1,14 @@
 package TesteAPIQAOPS;
 
 import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 import org.apache.http.HttpStatus;
 import org.junit.Test;
+
+import io.restassured.http.ContentType;
 
 public class AppTest {
     @Test
@@ -13,13 +16,26 @@ public class AppTest {
 
         when()
                 .get("https://reqres.in/api/users?page=2")
-               .then()
+                .then()
                 .statusCode(HttpStatus.SC_OK)
-                .body("data", is(notNullValue()))
-                // .body("page", is(2))
-                // .body("per_page", is(6))
-                // .body("data[0].email", is("michael.lawson@reqres.in"))
-                // .body("data[0].first_name", is("Michael"))
-                .statusCode(200);
+                .body("data", is(notNullValue()));
+        // .body("page", is(2))
+        // .body("per_page", is(6))
+        // .body("data[0].email", is("michael.lawson@reqres.in"))
+        // .body("data[0].first_name", is("Michael"))
+
+    }
+
+    @Test
+    public void testCriarUsuario() {
+        given()
+                .log()
+                .all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\":\"Fabio\", \"job\":\"Testador de Software\"}").when()
+                .post("https://reqres.in/api/users").then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("name", is("Fabio"));
+
     }
 }
