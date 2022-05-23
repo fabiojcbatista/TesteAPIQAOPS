998425cc258df64fc63ab14f4d8e2f56d07fd45f
fabiojcbatista <fabiojcbweb@gmail.com>
2022-05-23 17:30:23 -0300

    Insere log verboso para toda a classe

--- app/src/test/java/TesteAPIQAOPS/AppTest.java
+++ app/src/test/java/TesteAPIQAOPS/AppTest.java
@@ -1,41 +1,41 @@
 package TesteAPIQAOPS;
 
-import static io.restassured.RestAssured.when;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 import org.apache.http.HttpStatus;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 public class AppTest {
+    @BeforeClass
+    public static void setup(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
     @Test
     public void testListaMetadadosDoUsuario() {
-
         when()
-                .get("https://reqres.in/api/users?page=2")
-                .then()
-                .statusCode(HttpStatus.SC_OK)
-                .body("data", is(notNullValue()));
-        // .body("page", is(2))
-        // .body("per_page", is(6))
-        // .body("data[0].email", is("michael.lawson@reqres.in"))
-        // .body("data[0].first_name", is("Michael"))
-
+          .get("https://reqres.in/api/users?page=2").
+        then()
+          .statusCode(HttpStatus.SC_OK)
+          .body("data", is(notNullValue()));
     }
 
     @Test
     public void testCriarUsuario() {
         given()
-                .log()
-                .all()
-                .contentType(ContentType.JSON)
-                .body("{\"name\":\"Fabio\", \"job\":\"Testador de Software\"}").when()
-                .post("https://reqres.in/api/users").then()
-                .statusCode(HttpStatus.SC_CREATED)
-                .body("name", is("Fabio"));
-
+          .contentType(ContentType.JSON)
+          .body("{\"name\":\"Fabio\", \"job\":\"Testador de Software\"}").
+        when()
+          .post("https://reqres.in/api/users").
+        then()
+          .statusCode(HttpStatus.SC_CREATED)
+          .body("name", is("Fabio"));
     }
 }
